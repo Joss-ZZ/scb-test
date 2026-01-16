be019157978af46fc91e62f27301062baeb7608e
Joss-ZZ <65317295+Joss-ZZ@users.noreply.github.com>
2026-01-16 16:03:16 -0500

    Add author name to job parameter comment
    Updated job parameter comment to include author name.

--- src/monitor-plus-etl-scb.py
+++ src/monitor-plus-etl-scb.py
@@ -5,7 +5,7 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-## @params: [JOB_NAME] v2
+## @params: [JOB_NAME] v2 - Jhosimar Zevallos
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
 sc = SparkContext()
